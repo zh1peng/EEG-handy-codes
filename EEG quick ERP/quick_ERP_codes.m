03117d29069090f67c271042308d9d6abf5e04ea
懒麻蛇 <frueddy@gmail.com>
2018-01-02 13:12:09 +0000

    add contents

--- EEG quick ERP/quick_ERP_codes.m
+++ EEG quick ERP/quick_ERP_codes.m
@@ -1,7 +1,4 @@
-%test intermediate files
-addpath('V:\my code\TEST CODE')
-load('V:\my code\TEST CODE\chanlocs.mat')
-% chanlocs(69:70)=[];
+%prepare data to analyses
 data_path='W:\64 EEG\EEG_PROJECTS\eMID data Zhipeng\Final';
 [path,filename]=filesearch_regexp(data_path,'^Final\w*.set$');
 
@@ -30,14 +27,47 @@ data=[];
 end
 ORIG=[];
 end
-marks={'101','102','103','13','16','23','26','33','36'}
-        for marki=1:length(marks)
-    eval(sprintf('m%s=squeeze(mean(event_%s,3));',marks{marki},marks{marki}));
-        end
+
+
+
+
+addpath('F:\Google Drive\zhipeng git folders\my-EEG-codes\EEG quick ERP')
+load('F:\Google Drive\zhipeng git folders\my-EEG-codes\EEG quick ERP\chanlocs.mat')
+chanlocs(69:70)=[];
+
+% remove any bad sub if needed:
+% marks={'101','102','103','10150','10250','10350','13','16','23','26','33','36'}
+% for marki=1:length(marks)
+%     eval(sprintf('event_%s(:,:,17)=[];',marks{marki}));
+% end
         
+
+ for marki=1:length(marks)
+    eval(sprintf('m%s=squeeze(mean(event_%s,3));',marks{marki},marks{marki}));
+ end
+
+
+ 
+ 
+%% EMID info	
+%   101	'green cue'
+% 	102	'red cue'
+% 	103	'blue cue'
+% 	50	'target'
+% 	13	'green positive feedback'
+% 	16	'green negative feedback'
+% 	23	'red positive feedback'
+% 	26	'red negative feedback'
+% 	33	'blue positive feedback'
+% 	36	'blue negative feedback'
+%  srate=512 % 1280/2500=0.512
+
+ 
+ 
+ %% cue P3
 Fnorm=12/(512/2)
 df=designfilt('lowpassfir','FilterOrder',15,'CutoffFrequency',Fnorm)
-for chan=38
+for chan=32
 % chan_label=chaninfo(chan).labels;
 
  x=1:1126;
@@ -59,7 +89,6 @@ set(gca,'YDir','reverse')
 set(gca,'xtick',[0:51:1126],'xticklabel',[-200:100:2000])
 end
 
-chanlocs(end-1:end)=[];
 figure; topoplot(squeeze(mean(m101(:,282:409),2)),chanlocs);
 title(num2str('green cue 350-600'))
 
@@ -68,3 +97,90 @@ title(num2str('red cue 350-600'))
 
 figure; topoplot(squeeze(mean(m103(:,282:409),2)),chanlocs);
 title(num2str('blue cue 350-600'))
+
+
+%% target P3
+Fnorm=12/(512/2)
+df=designfilt('lowpassfir','FilterOrder',15,'CutoffFrequency',Fnorm)
+for chan=32
+% chan_label=chaninfo(chan).labels;
+
+ x=1:1126;
+       x1=m10150(chan,x)%-mean(m101(chan,1:102));
+       x2=m10250(chan,x)%-mean(m102(chan,1:102));
+       x3=m10350(chan,x)%-mean(m103(chan,1:102));
+xx = 0:1:1125;
+xx1 = filter(df,x1)
+xx2= filter(df,x2)
+xx3= filter(df,x3)
+figure
+plot(xx,xx1,'g',xx,xx2,'r',xx,xx3,'b');
+hline(0,'b-')
+vline(102,'b-')
+ legend('green cue','red cue',	'blue cue')
+ title(chanlocs(chan).labels)
+ ylim([-10,30])
+set(gca,'YDir','reverse')   
+set(gca,'xtick',[0:51:1126],'xticklabel',[-200:100:2000])
+end
+
+figure; topoplot(squeeze(mean(m10150(:,282:409),2)),chanlocs);
+title(num2str('green target 350-600'))
+
+figure; topoplot(squeeze(mean(m10250(:,282:409),2)),chanlocs);
+title(num2str('red target 350-600'))
+
+figure; topoplot(squeeze(mean(m10350(:,282:409),2)),chanlocs);
+title(num2str('blue target 350-600'))
+
+%% FB analyses
+%1 . omitted gain vs. actual loss vs. neutral loss
+% 16 vs. 26 vs. 36
+Fnorm=12/(512/2)
+df=designfilt('lowpassfir','FilterOrder',15,'CutoffFrequency',Fnorm)
+for chan=32
+% chan_label=chaninfo(chan).labels;
+
+ x=1:1126;
+       x1=m16(chan,x)%-mean(m101(chan,1:102));
+       x2=m26(chan,x)%-mean(m102(chan,1:102));
+       x3=m36(chan,x)%-mean(m103(chan,1:102));
+xx = 0:1:1125;
+xx1 = filter(df,x1)
+xx2= filter(df,x2)
+xx3= filter(df,x3)
+figure
+plot(xx,xx1,'g',xx,xx2,'r',xx,xx3,'b');
+hline(0,'b-')
+vline(102,'b-')
+ legend('omitted gain', 'actual loss',	'neutral loss')
+ title(chanlocs(chan).labels)
+ ylim([-10,30])
+set(gca,'YDir','reverse')   
+set(gca,'xtick',[0:51:1126],'xticklabel',[-200:100:2000])
+end
+
+
+%2. actual gain  vs. avoided loss vs. neutral gain
+% 13 vs.23 vs.33
+for chan=32
+% chan_label=chaninfo(chan).labels;
+
+ x=1:1126;
+       x1=m13(chan,x)%-mean(m101(chan,1:102));
+       x2=m23(chan,x)%-mean(m102(chan,1:102));
+       x3=m33(chan,x)%-mean(m103(chan,1:102));
+xx = 0:1:1125;
+xx1 = filter(df,x1)
+xx2= filter(df,x2)
+xx3= filter(df,x3)
+figure
+plot(xx,xx1,'g',xx,xx2,'r',xx,xx3,'b');
+hline(0,'b-')
+vline(102,'b-')
+ legend('actual gain', 'avoided loss',	'neutral gain')
+ title(chanlocs(chan).labels)
+ ylim([-10,30])
+set(gca,'YDir','reverse')   
+set(gca,'xtick',[0:51:1126],'xticklabel',[-200:100:2000])
+end
